1f7e3f9f978be92798cc48ddec76c8cea3d72e47
ehk-kim <eh.kim4500@gmail.com>
2025-01-14 11:45:19 -0500

    finished treeDistances.R code

--- reference_trees/treeDistances.R
+++ reference_trees/treeDistances.R
@@ -7,14 +7,13 @@
 # script will not run without the JGItree.nwk and Lietal_2021.nwk files available
 # in this folder.
 # 
-# Use: treeDistances.R input output
+# Use: Rscript treeDistances.R input output
 #      input: tree in Newick file format
 #      output: file name you want to output the results to. if the file exists,
 #              outputs will be appended. if not, file will be created.
 #####
 
 # Command line arguments
-
 args <- commandArgs(trailingOnly = TRUE)
 
 # Wrong number of arguments
@@ -23,12 +22,49 @@ if(length(args) < 2){
   stop("Requires command line argument.")
 }
 
+fin <- args[1]
+fout <- args[2]
+
 # Load all libraries
 library(TreeDist)
 library(ape)
 
-
+# Read in reference trees
 jgi <- read.tree("JGItree.nwk")
-# li <- read.tree("Lietal_2021.nwk")
+li <- read.tree("Lietal_2021.nwk")
 
-input_file <- read.tree()
+# Read in comparison tree
+input_tree <- read.tree(fin)
+
+# Compute the Nye similarity scores
+nye_jgi <- NyeSimilarity(jgi, input_tree, normalize = pmax.int)
+nye_li <- NyeSimilarity(li, input_tree, normalize = pmax.int)
+
+#####
+# normalize = pmax.int normalizes against the number of splits in the most
+# resolved tree; in this case, the references
+#
+# Nye similarity is a generalized Robinson-Foulds metric. It finds the optimal
+# matching that pairs each branch from one tree with a branch in the second and
+# scores matches according to the size of the largest split that is
+# consistent with both of them. It is then normalized against the Jaccard index.
+#
+# Since the resulting scores are similarity scores, they range from 0-1 with 1
+# being a perfect match.
+#####
+
+# Output the results to a file
+jgi_text <- "compared with JGI\t"
+li_text <- "compared with Li et al. 2021\t"
+
+if(file.exists(fout)) {
+  conx <- file(fout, "a")
+  cat(fin, "\t", jgi_text, nye_jgi, "\n", file = conx)
+  cat(fin, "\t", li_text, nye_li, "\n", file = conx)
+  close(conx)
+} else {
+  cat(fin, "\t", jgi_text, nye_jgi, "\n", file = fout)
+  conx <- file(fout, "a")
+  cat(fin, "\t", li_text, nye_li, "\n", file = conx)
+  close(conx)
+}
